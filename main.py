459d9f68127785de8dbb2c0c7c302a8863cb1607
neo-Angelo <131836989+neo-Angelo@users.noreply.github.com>
2025-07-21 16:17:28 -0300

    tentativa de mandar mensagem

--- main.py
+++ main.py
@@ -109,6 +109,31 @@ class PacienteComUsuario(BaseModel):
 class AgendaUpdateStatus(BaseModel):
     status: str
 
+class ConnectionManager:
+    def __init__(self):
+        # Dicionário para manter as conexões ativas: {usuario_id: websocket}
+        self.active_connections: dict[int, WebSocket] = {}
+
+    async def connect(self, websocket: WebSocket, user_id: int):
+        await websocket.accept()
+        self.active_connections[user_id] = websocket
+
+    def disconnect(self, user_id: int):
+        if user_id in self.active_connections:
+            del self.active_connections[user_id]
+
+    async def send_personal_message(self, message: str, user_id: int):
+        if user_id in self.active_connections:
+            websocket = self.active_connections[user_id]
+            await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections.values():
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
+
 # ==== USUARIO ====
 @app.post("/pacientes-com-usuario")
 def cadastrar_paciente_com_usuario(dados: PacienteComUsuario):
